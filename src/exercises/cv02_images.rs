786f7e5ebbcc4c2a78ab55f36ce92dc2e20bd807
Radim Kotajny <radim.kotajny@gmail.com>
2025-10-09 18:19:13 +0200

    added brackets to luminance calculation for better visibility

--- src/exercises/cv02_images.rs
+++ src/exercises/cv02_images.rs
@@ -6,7 +6,7 @@ pub fn grayscale(vram: &mut VRam) {
         for x in 0..vram.width {
             if let Some((r, g, b)) = vram.get_pixel_rgb(x, y) {
                 // Compute luminance using weighted average (no division by 3)
-                let l = 0.299 * r as f32 + 0.587 * g as f32 + 0.114 * b as f32;
+                let l = (0.299 * r as f32) + (0.587 * g as f32) + (0.114 * b as f32);
                 vram.set_pixel(x, y, l as u8, l as u8, l as u8);
             }
         }
