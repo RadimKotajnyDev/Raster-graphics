29a500bc83b364c7619eb72f41f70f281b058484
Radim Kotajny <radimkotajny@gmail.com>
2025-12-07 20:37:47 +0100

    Improve `draw_line` to handle single-point lines and edge boundary checks

--- src/utils/drawing.rs
+++ src/utils/drawing.rs
@@ -4,8 +4,14 @@ use crate::vram::VRam;
 pub fn draw_line(vram: &mut VRam, p1: Point, p2: Point, r: u8, g: u8, b: u8) {
     let dx = p2.x - p1.x;
     let dy = p2.y - p1.y;
-
     let steps = dx.abs().max(dy.abs());
+
+    if steps <= f32::EPSILON {
+        if p1.x >= 0.0 && p1.x < vram.width as f32 && p1.y >= 0.0 && p1.y < vram.height as f32 {
+            vram.set_pixel(p1.x.round() as u32, p1.y.round() as u32, r, g, b);
+        }
+        return;
+    }
 
     let x_inc = dx / steps;
     let y_inc = dy / steps;
@@ -13,10 +19,13 @@ pub fn draw_line(vram: &mut VRam, p1: Point, p2: Point, r: u8, g: u8, b: u8) {
     let mut x = p1.x;
     let mut y = p1.y;
 
-    for _i in 1..=(steps.ceil() as u32) {
+    if x >= 0.0 && x < vram.width as f32 && y >= 0.0 && y < vram.height as f32 {
+        vram.set_pixel(x.round() as u32, y.round() as u32, r, g, b);
+    }
+
+    for _ in 0..steps.ceil() as u32 {
         x += x_inc;
         y += y_inc;
-
         if x >= 0.0 && x < vram.width as f32 && y >= 0.0 && y < vram.height as f32 {
             vram.set_pixel(x.round() as u32, y.round() as u32, r, g, b);
         }
